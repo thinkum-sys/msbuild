7b9ee64ba6606961356393af6e7bd9fdcc19ae70
Ankit Jain <ankit.jain@xamarin.com>
2016-04-01 11:40:09 -0400

    Enable Binary serialization for Mono builds also.
    This enables resgen caches.
    
    Also mark Microsoft.Build.UnitTests.GenerateResource*_Tests.InProc.PropertyErrors.StateFileUnwritable
    as windows only. This depends on setting the cache file read-only, and
    then expecting error when trying to delete it. But it is still legal for
    the file owner to delete it on Unix.

--- src/XMakeBuildEngine/Errors/InvalidToolsetDefinitionException.cs
+++ src/XMakeBuildEngine/Errors/InvalidToolsetDefinitionException.cs
@@ -95,7 +95,9 @@ namespace Microsoft.Build.Exceptions
         /// </summary>
         /// <param name="info"></param>
         /// <param name="context"></param>
+#if FEATURE_SECURITY_PERMISSIONS
         [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+#endif
         override public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             ErrorUtilities.VerifyThrowArgumentNull(info, "info");
